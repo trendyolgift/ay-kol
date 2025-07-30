ab3c39afe750f63d4b354728ace085b1ae3bb7f4
beachcumen <beachcumen@gmail.com>
2025-07-30 13:33:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,1 @@
-import telebot
 
-bot = telebot.TeleBot("8261687996:AAHKxy5AuX-CS0aavPUCEllNq1MGyQ4CwDY")
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Bot çalışıyor koçum.")
-
-bot.polling()
